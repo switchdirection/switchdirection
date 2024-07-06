96221bc8876221467e1ef5f71340994dc53283a1
switchdirection <gordon-play@bk.ru>
2024-07-06 15:38:12 +0300

    Finish Manual Tasks

--- TasksFromManualHomeWork5/Program.cs
+++ TasksFromManualHomeWork5/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace TasksFromManualHomeWork5
+﻿using System.Text;
+
+namespace TasksFromManualHomeWork5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string argument = "1234-abc-5678-def-9g0h";
+            ManualTasks.EightFirstDigit(argument);
+            ManualTasks.ReplaceLetters(argument);
+            ManualTasks.LowerCaseLettersInFormat(argument);
+            ManualTasks.UpperCaseLettersInFormat(argument);
+            ManualTasks.CheckIfAStringStartingWithASequence(argument);
         }
+
+        
     }
 }
