07c0c5484e72dd03fac8b10d0f3c952ad67af784
switchdirection <gordon-play@bk.ru>
2024-07-10 11:37:18 +0300

    Add new project, and rename old

---
+++ AdditionalTaskFromManualHomeWork6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdditionalTaskFromManualHomeWork6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
